79cc80836846e79d5953d926a3652b0a33da7d97
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-10-20 20:23:56 +0300

    Added the two new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { RepoComponent } from './pages/repo/repo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/',
+    component: SearchComponent,
+  },
+  {
+    path: '/repo/{id}',
+    component: RepoComponent,
+  },
+];
---
+++ src/app/pages/repo/repo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-repo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './repo.component.html',
+  styleUrls: ['./repo.component.css']
+})
+export class RepoComponent {
+
+}
