5f1c957aa42a73f38d444d4a1557cc9fcff2afc8
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-10-21 15:18:39 +0300

    Added a new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './pages/search/search.component';
 import { RepoComponent } from './pages/repo/repo.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'search',
     component: SearchComponent,
   },
   {
