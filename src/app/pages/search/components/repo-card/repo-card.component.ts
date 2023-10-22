f721b4021dd3db97db0bcad040f1292b670d6ac4
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-10-22 05:54:06 +0300

    Injected the response data into view

--- src/app/pages/search/components/repo-card/repo-card.component.ts
+++ src/app/pages/search/components/repo-card/repo-card.component.ts
@@ -12,18 +12,5 @@ import { Repo } from '../../models/repos-list.model';
   styleUrls: ['./repo-card.component.css'],
 })
 export class RepoCardComponent {
-  @Input() repo: Repo = {
-    owner: {
-      name: 'nasa',
-      avatarUrl:
-        'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg',
-    },
-    repoName: 'fprime',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id voluptate aspernatur.',
-    language: 'javascript',
-    stars: 0,
-    forks: 0,
-    issues: 0,
-  };
+  @Input() repo!: Repo;
 }
